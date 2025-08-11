d03a5d93dc5c327269139a3a71f81f0c0a3df652
sreelakshmi-11 <sreelakshmi@ctruh.com>
2025-08-11 15:20:59 +0530

    updated json

--- client/src/context/appContext.jsx
+++ client/src/context/appContext.jsx
@@ -3,12 +3,15 @@ import { useState, useEffect } from "react";
 import { dummyCourses } from "../assets/assets";
 import { useNavigate } from "react-router-dom";
 import humanizeDuration from "humanize-duration";
+import { useAuth, useUser } from "@clerk/clerk-react";
 
 export const AppContext = createContext();
 
 export const AppContextProvider = (props) => {
   const currency = import.meta.env.VITE_CURRENCY;
   const navigate = useNavigate();
+  const { getToken } = useAuth();
+  const { user } = useUser();
   const [allCourses, setAllCourses] = useState([]);
   const [isEducator, setIsEducator] = useState(true);
   const [enrolledCourses, setEnrolledCourses] = useState([]);
@@ -65,6 +68,16 @@ export const AppContextProvider = (props) => {
     fetchAllCourses();
     getEnrolledCourses();
   }, []);
+
+  const logToken = async () => {
+    console.log(await getToken());
+  };
+
+  useEffect(() => {
+    if (user) {
+      logToken();
+    }
+  }, [user]);
 
   const value = {
     currency,
