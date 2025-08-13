6a8e414ae0176697ae595679ba73155b72f2d033
sreelakshmi-11 <sreelakshmi@ctruh.com>
2025-08-13 19:09:57 +0530

    fixed mongo

--- server/configs/mongodb.js
+++ server/configs/mongodb.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   mongoose.connection.on("connected", () => console.log("Database connected"));
-  await mongoose.connect(`${process.env.MONGO_URI}`);
+  await mongoose.connect(`${process.env.MONGO_URI}/lms`);
 };
 
 export default connectDB;
